31a9faf278be99b28a3954633829a2a5b8f1bf9e
marquis-d-ugo <munachimso.ugorji@andela.com>
2019-10-20 21:09:37 +0100

    getting picker

--- xChange/controllers/XChangeViewController.swift
+++ xChange/controllers/XChangeViewController.swift
@@ -1,12 +1,51 @@
 import UIKit
 
 class XChangeViewController: UIViewController {
-    @IBOutlet weak var mainCurrencyTextField: UITextField!
-    @IBOutlet weak var currencyToBeChangeTextField: UITextField!
+    @IBOutlet private weak var mainCurrencyTextField: UITextField!
+    @IBOutlet private weak var currencyToBeChangeTextField: UITextField!
+    @IBOutlet private weak var currencyButtonNameLabel: UILabel!
+    @IBOutlet private weak var currencyButtonImageView: UIImageView!
+    
+    private var viewModel: XChangeViewModel?
+    
+    private var toolBar = UIToolbar()
+    private var pickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
+        self.viewModel = XChangeViewModel()
+        self.bindViewModel(with: viewModel)
     }
+    
+    func bindViewModel(with viewModel: BaseViewModel?) {
+        guard let viewModel = viewModel as? XChangeViewModel else { return }
+        
+    }
+    
+    
+    @IBAction func onClickSelectCurrency(_ sender: UIButton) {
+        pickerView = UIPickerView.init()
+        pickerView.delegate = self
+        pickerView.backgroundColor = UIColor.white
+        pickerView.setValue(UIColor.black, forKey: "textColor")
+        pickerView.autoresizingMask = .flexibleWidth
+        pickerView.contentMode = .center
+        pickerView.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(pickerView)
+
+        toolBar = UIToolbar.init(frame: CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
+        toolBar.barStyle = .blackTranslucent
+        toolBar.items = [UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(onDoneButtonTapped))]
+        self.view.addSubview(toolBar)
+    }
+    
+    @objc func onDoneButtonTapped() {
+        toolBar.removeFromSuperview()
+        pickerView.removeFromSuperview()
+    }
+    
 }
 
 extension XChangeViewController: XChangeDelegate {
@@ -15,6 +54,22 @@ extension XChangeViewController: XChangeDelegate {
     
     func onFail(error: String) {
         print(error)
+    }
+}
+
+extension XChangeViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        guard let vModel = self.viewModel else { return 0 }
+        return vModel.getItemsCount()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        guard let vModel = self.viewModel else { return String.empty }
+        return vModel.getEachItem(index: row)
     }
 }
 
--- xChange/viewModels/XChangeViewModel.swift
+++ xChange/viewModels/XChangeViewModel.swift
@@ -1,6 +1,25 @@
 import Foundation
 
 class XChangeViewModel: BaseViewModel {
-    
+    private var items = ["this", "that", "which", "just", "gone"]
 }
 
+extension XChangeViewModel: XChangeVMProtocol {
+    func getEachItem(index: Int) -> String {
+        return self.getItems()[index]
+    }
+    
+    func getItemsCount() -> Int {
+        return self.getItems().count
+    }
+    
+    func getItems() -> [String] {
+        return self.items
+    }
+}
+
+protocol XChangeVMProtocol {
+    func getItems() -> [String]
+    func getItemsCount() -> Int
+    func getEachItem(index: Int) -> String
+}
